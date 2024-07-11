f2af677e118f358061430a776a507bbd3df84fdf
wakewon <40191996+wakewon@users.noreply.github.com>
2024-07-11 15:31:51 +0800

    更新 appcast

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 cd src
 zip ../bob-plugin-simpletex-v.bobplugin.zip *.js *.json *.png
-timestamp=$(date +%s)
-echo "$timestamp"
+timestamp=$(date +%s%N)
+timestamp=${timestamp:0:10}${timestamp:13:3}
+echo $timestamp
 cd ..
 shasum -a 256 bob-plugin-simpletex-v.bobplugin
