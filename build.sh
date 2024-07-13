dfcb41c6139e06eb1d1b2d6542665a67beeee5df
wakewon <40191996+wakewon@users.noreply.github.com>
2024-07-13 21:29:46 +0800

    Remove .zip

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ fi
 cd src
 
 # 生成插件zip文件
-zip ../bob-plugin-simpletex-v$version.bobplugin.zip *.js *.json *.png
+zip ../bob-plugin-simpletex-v$version.bobplugin *.js *.json *.png
 
 # 生成时间戳
 timestamp=$(($(date +%s)*1000))
@@ -27,14 +27,14 @@ timestamp=$(($(date +%s)*1000))
 cd ..
 
 # 计算sha256值
-sha256=$(shasum -a 256 bob-plugin-simpletex-v$version.bobplugin.zip | awk '{print $1}')
+sha256=$(shasum -a 256 bob-plugin-simpletex-v$version.bobplugin | awk '{print $1}')
 
 # 更新appcast.json中与提取的版本号相匹配的项，如果不存在则添加新项
 jq --arg version "$version" --arg sha256 "$sha256" --argjson timestamp $timestamp '
     if (.versions | map(.version) | index($version)) then
         .versions |= map(if .version == $version then .sha256 = $sha256 | .timestamp = $timestamp else . end)
     else
-        .versions = [{"version": $version, "desc": "更新日志","sha256": $sha256, "url": "https://github.com/wakewon/bob-plugin-simpletex/releases/download/v\($version)/bob-plugin-simpletex-v\($version).bobplugin.zip", "minBobVersion": "0.5.0","timestamp": $timestamp}] + .versions
+        .versions = [{"version": $version, "desc": "更新日志","sha256": $sha256, "url": "https://github.com/wakewon/bob-plugin-simpletex/releases/download/v\($version)/bob-plugin-simpletex-v\($version).bobplugin", "minBobVersion": "0.5.0","timestamp": $timestamp}] + .versions
     end
 ' appcast.json > tmp.json && mv tmp.json appcast.json
 
